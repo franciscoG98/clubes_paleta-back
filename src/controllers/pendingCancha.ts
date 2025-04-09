5e2641f890095a73d7c25eee845a62b6bf64c5fa
franciscog98 <franciscomanuel.gonzalez3@gmail.com>
2025-04-09 14:04:36 -0300

    feat: docs with swagger for pending cancha, fix img route for create pending cancha

--- src/controllers/pendingCancha.ts
+++ src/controllers/pendingCancha.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from 'express';
+import { Handler } from 'express';
 import db from '../models';
 import multer from 'multer';
 import { Cancha } from '../models/cancha';
@@ -22,112 +22,120 @@ interface Filter {
   pending?: boolean;
 }
 
-export class PendingCanchaController {
+export const getAllPendingCanchas: Handler = async (req, res) => {
+  try {
+    const { pending } = req.query;
 
-  static async getAllPendingCanchas(req: Request, res: Response) {
-    try {
-      const { pending } = req.query;
+    const filters: Filter = {};
+    if (typeof pending === 'boolean') filters.pending = pending;
 
-      const filters: Filter = {};
-      if (typeof pending === 'boolean') filters.pending = pending;
+    const pendingCanchas = await PendingCancha.findAll({
+      where: filters,
+    });
 
-      const pendingCanchas = await PendingCancha.findAll({
-        where: filters,
-      });
-
-      res.json(pendingCanchas);
-    } catch (error) {
-      console.error(error);
-      res.status(500).send('Error fetching pending canchas');
-    }
+    res.json(pendingCanchas);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error fetching pending canchas');
   }
+}
 
-  static async createPendingCancha(req: Request, res: Response) {
+export const createPendingCancha: Handler = async (req, res) => {
+  const { club, city, state, type, maps_location, phone } = req.body;
 
-    const { club, city, state, type, maps_location, phone } = req.body;
+  // Si no se subió archivo y no hay imagen en el body, usar la imagen por defecto
+  const image = req.file ? `/uploads/${req.file.filename}` : DEFAULT_IMAGE_PATH;
 
-    // Si no se subió archivo y no hay imagen en el body, usar la imagen por defecto
-    const image = req.file ? req.file.filename : DEFAULT_IMAGE_PATH;
+  try {
+    const newPendingCancha = await PendingCancha.create({
+      club,
+      city,
+      state,
+      type,
+      maps_location,
+      phone,
+      image
+    });
 
-    try {
-      const newPendingCancha = await PendingCancha.create({
-        club,
-        city,
-        state,
-        type,
-        maps_location,
-        phone,
-        image
-      });
-
-      res.status(201).json(newPendingCancha);
-    } catch (error) {
-      console.error(error);
-      res.status(400).send('The cancha could not be created');
-    }
+    res.status(201).json(newPendingCancha);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('The cancha could not be created');
   }
+}
 
-  static async approvePendingCancha(req: Request, res: Response): Promise<void>  {
-    // const { id } = req.query
+export const approvePendingCancha: Handler = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const pendingCancha = await PendingCancha.findOne({ where: { id: Number(id) } })
+
+    if (!pendingCancha) {
+      res.status(404).json({ message: "Pending cancha not found" })
+    }
+
+    // Parse and clean
+    const pendingCanchaData = pendingCancha.toJSON()
+    delete pendingCanchaData.id
+    delete pendingCanchaData.createdAt
+    delete pendingCanchaData.updatedAt
+
+    // Create a new cancha with the pending cancha data
+    const newCancha = await Cancha.create(pendingCanchaData)
+
+    // Delete the pending cancha
+    await PendingCancha.destroy({ where: { id: Number(id) } })
+
+    res.status(200).json({
+      message: "Cancha approved successfully",
+      cancha: newCancha,
+    })
+  } catch (error) {
+    console.error(error)
+    res.status(500).send("Error approving cancha")
+  }
+}
+
+export const updatePendingCancha: Handler = async (req, res) =>{
+  try {
     const { id } = req.params;
+    const { pending } = req.body;
 
-    try {
-      // Find the pending cancha
-      const pendingCancha = await PendingCancha.findOne({ where: { id: Number(id) } })
-
-      if (!pendingCancha) {
-        res.status(404).json({ message: "Pending cancha not found" })
-      }
-
-      // Get the data from the pending cancha
-      const pendingCanchaData = pendingCancha.toJSON()
-
-      // Remove any Sequelize-specific fields that shouldn't be copied
-      delete pendingCanchaData.id
-      delete pendingCanchaData.createdAt
-      delete pendingCanchaData.updatedAt
-
-      // Create a new cancha with the pending cancha data
-      const newCancha = await Cancha.create(pendingCanchaData)
-
-      // Delete the pending cancha
-      await PendingCancha.destroy({ where: { id: Number(id) } })
-
-      res.status(200).json({
-        message: "Cancha approved successfully",
-        cancha: newCancha,
-      })
-    } catch (error) {
-      console.error(error)
-      res.status(500).send("Error approving cancha")
+    if (!id) {
+      res.status(404).json({ error: 'Cancha no encontrada' });
     }
+
+    const updatedCancha = await PendingCancha.update(
+      { pending }, 
+      { where: { id: Number(id) } }
+    );
+
+    if (updatedCancha[0] === 0) {
+      res.status(404).json({ error: 'No se encontró la cancha o el valor no cambió' });
+    }
+
+    res.status(200).json({ message: 'Cancha actualizada con éxito' });
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error updating cancha');
   }
-  // ---------------------------------
+}
 
-  static async updatePendingCancha(req: Request, res: Response): Promise<void> {
-    try {
-      const { id } = req.params;
-      const { pending } = req.body;
+export const deletePendingCancha: Handler = async (req, res) =>{
+  try {
+    const { id } = req.params;
+    const pendingCanchaToRemove = await PendingCancha.destroy({ where: { id } })
 
-      if (!id) {
-        res.status(404).json({ error: 'Cancha no encontrada' });
-      }
-
-      const updatedCancha = await PendingCancha.update(
-        { pending }, 
-        { where: { id: Number(id) } }
-      );
-
-      if (updatedCancha[0] === 0) {
-        res.status(404).json({ error: 'No se encontró la cancha o el valor no cambió' });
-      }
-
-      res.status(200).json({ message: 'Cancha actualizada con éxito' });
-
-    } catch (error) {
-      console.error(error);
-      res.status(400).send('The cancha could not be updated');
+    if (!pendingCanchaToRemove) {
+      res.status(404).json({ message: 'Pending cancha not found' });
+      return;
     }
+
+    res.status(204).json({ message : 'Pending cancha successfully deleted' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error deleting pending cancha');
   }
 }
 
--- src/routes/pendingCancha.ts
+++ src/routes/pendingCancha.ts
@@ -1,12 +1,211 @@
 import { Router } from 'express';
-import { PendingCanchaController, uploadMiddleware } from "../controllers/pendingCancha"
-// import PendingCanchaController, { uploadMiddleware } from "../controllers/pendingCancha"
+import {
+    getAllPendingCanchas,
+    createPendingCancha,
+    approvePendingCancha,
+    updatePendingCancha,
+    deletePendingCancha,
+    uploadMiddleware
+} from "../controllers/pendingCancha"
 
 const router = Router();
 
-router.post("/", uploadMiddleware, PendingCanchaController.createPendingCancha);
-router.get('/', PendingCanchaController.getAllPendingCanchas);
-router.put('/:id', PendingCanchaController.updatePendingCancha);
-router.post('/approve-cancha/:id', PendingCanchaController.approvePendingCancha)
+/**
+ *  @swagger
+ *    components:
+ *      schemas:
+ *          PendingCancha:
+ *              type: object
+ *              properties:
+ *                  id:
+ *                      type: number
+ *                      description: the id of the cancha
+ *                  club:
+ *                      type: string
+ *                      description: the name of the cancha
+ *                  city:
+ *                      type: string
+ *                      description: the city of the cancha
+ *                  state:
+ *                      type: string
+ *                      description: the state of the cancha
+ *                  type:
+ *                      type: string
+ *                      description: the type of the cancha, Trinquete, Frontón or Cajón
+ *                  maps_location:
+ *                      type: string
+ *                      description: the direction of the cancha
+ *                  phone:
+ *                      type: string
+ *                      description: the phone of the cancha
+ *                  image:
+ *                      type: string
+ *                      description: the image of the cancha (path ?)
+ *                  pending:
+ *                      type: boolean
+ *                      description: if the cancha needs to be rejected or approved
+ *              required:
+ *                  - club
+ *                  - city
+ *                  - state
+ *                  - type
+ *                  - maps_location
+ *                  - phone
+ *                  - pending
+ *                  - image
+ *              example:
+ *                  id: 1
+ *                  club: Club Nahuel Huapi
+ *                  city: Bariloche
+ *                  state: Rio Negro
+ *                  type: Trinquete
+ *                  maps_location: Luelmo 38
+ *                  phone: 2944112233
+ *                  pending: true
+ *                  image: /uploads/cancha_default.webp
+ *          CanchaNotFound:
+ *              type: Object
+ *              properties:
+ *                  message:
+ *                      type: string
+ *                      description: the message for the error in fetching
+ *              example:
+ *                  message: Error fetching pending cancha
+ *      parameters:
+ *          pendingCanchaId:
+ *              in: path
+ *              name: id
+ *              required: true
+ *              schema: 
+ *                  type: string 
+ *              description: the pending cancha id
+ * 
+*/
+
+/**
+ * @swagger
+ *  tags:
+ *      name: Pending Canchas
+ *      description: Pending Canchas endpoint
+ */
+
+/**
+ * @swagger
+ * /pending-canchas:
+ *   get:
+ *     summary: Returns all the pending canchas in an array
+ *     tags: [Pending Canchas]
+ *     responses:
+ *       200:
+ *         description: all the pending canchas list
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                  $ref: '#/components/schemas/PendingCancha'
+ *       500:
+ *         description: No hay canchas
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/CanchaNotFound'
+ */
+router.get('/', getAllPendingCanchas);
+
+/**
+* @swagger
+*   /pending-canchas:
+*     post:
+*       summary: Create a new pending cancha
+*       tags: [Pending Canchas]
+*       requestBody:
+*         required: true
+*         content:
+*           application/json:
+*             schema:
+*               $ref: '#/components/schemas/Cancha'
+*         responses:
+*           201:
+*             description: the task was succesfully created
+*             content:
+*               application/json:
+*                 schema:
+*                   $ref: '#/components/schemas/PendingCancha'
+*           500:
+*             description: some server error
+*             content:
+*               application/json:
+*                 schema:
+*                   $ref: '#/components/schemas/CanchaNotFound'
+*/
+router.post("/", uploadMiddleware, createPendingCancha);
+
+/**
+* @swagger
+*   /pending-canchas/approve-cancha/{id}:
+*     post:
+*       summary: Approve a pending cancha and create a Cancha from it
+*       tags: [Pending Canchas]
+*       parameters:
+*         - $ref: '#/components/parameters/pendingCanchaId'
+*       responses:
+*         200:
+*           description: the task was succesfully created
+*           content:
+*             application/json:
+*               schema:
+*                 $ref: '#/components/schemas/PendingCancha'
+*         404:
+*             description: the cancha to approve was not found
+*             content:
+*               application/json:
+*                 schema:
+*                   $ref: '#/components/schemas/CanchaNotFound'
+*         500:
+*           description: some server error
+*           content:
+*             application/json:
+*               schema:
+*                 $ref: '#/components/schemas/CanchaNotFound'
+*/
+router.post('/approve-cancha/:id', approvePendingCancha)
+
+// TODO: not implemented on client
+router.put('/:id', updatePendingCancha);
+
+/**
+ * @swagger
+ * /pending-canchas/{id}:
+ *    delete:
+ *      summary: Delete pending cancha by id
+ *      tags: [Pending Canchas]
+ *      parameters:
+ *          - $ref: '#/components/parameters/pendingCanchaId'
+ *      responses:
+ *        204:
+ *          description: deleted pending cancha
+ *          content:
+ *            application/json:
+ *              schema:
+ *                  type: Object
+ *                  properties:
+ *                      message:
+ *                          type: string
+ *                          value: 'Pending cancha deleted'
+ *        404:
+ *          description: No se encontro la cancha
+ *          content:
+ *            application/json:
+ *              schema:
+ *                  $ref: '#/components/schemas/CanchaNotFound'
+ *        500:
+ *          description: No hay canchas
+ *          content:
+ *            application/json:
+ *              schema:
+ *                $ref: '#/components/schemas/CanchaNotFound'
+ */
+router.delete('/:id', deletePendingCancha)
 
 export default router;
